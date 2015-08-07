0f660cee1783b4d845cd03fb55efe75cb3dee136
Denis Izmaylov <izmaylov.dm@gmail.com>
2015-08-07 16:30:08 +0300

    Add promise support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 export default function thunkMiddleware({ dispatch, getState }) {
-  return next => action =>
-    typeof action === 'function' ?
-      action(dispatch, getState) :
-      next(action);
+  return next => action => {
+    if (typeof action === 'function') {
+      const result = action(dispatch, getState);
+      if (typeof result === 'object' && result.then === 'function') {
+        return result.then(next);
+      }
+      return result;
+    }
+    return next(action);
+  }
 }
